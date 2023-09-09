229712f0f58e8c62445823c6489efaa3bdcba337
Achim Kuntz <achimobil@hotmail.com>
2023-09-09 16:19:05 +0200

    cmd changes

--- Test.Ostsee.2.cmd
+++ Test.Ostsee.2.cmd
@@ -1,2 +1,2 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip "E:\LS22Profile\Ostseekueste\mods\FS22_ProductionInfoHud.zip" @FileDefinition.txt
+"C:\Program Files\7-Zip\7z.exe" u -tzip "E:\LS22Profile\Ostseekueste\mods\FS22_ProductionInfoHud.zip" @FileDefinition.txt
 start "" "C:\Program Files (x86)\Farming Simulator 2022\x64\FarmingSimulator2022Game.exe" -autoStartSavegameId 2 -cheats -profile E:\LS22Profile\Ostseekueste -devWarning
